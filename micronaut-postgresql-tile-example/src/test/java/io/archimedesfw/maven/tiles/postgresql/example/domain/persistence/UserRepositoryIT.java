009754a758b3a2bb7bbe14f261a248a85b14d193
cgmarcos <cgmarcos@autentia.com>
2021-09-27 17:38:32 +0200

    Initial micronaut postgresql tile example

---
+++ micronaut-postgresql-tile-example/src/test/java/io/archimedesfw/maven/tiles/postgresql/example/domain/persistence/UserRepositoryIT.java
@@ -0,0 +1,67 @@
+package io.archimedesfw.maven.tiles.postgresql.example.domain.persistence;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import io.archimedesfw.maven.tiles.postgresql.example.domain.User;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+
+@MicronautTest
+class UserRepositoryIT{
+
+    private final static User USER_NEW   = new User(3L, "ffernandez", "Federico", "Fernandez");
+    private final static User USER_TEST1 = new User(1L, "jdoe", "John", "Doe");
+    private final static User USER_TEST2 = new User(2L, "asmith", "Addam", "Smith");
+
+    @Inject
+    UserRepository userRepositorySUT;
+
+    @Test
+    void shouldSaveUser() {
+
+        var result = userRepositorySUT.save(USER_NEW);
+
+        assertThat(result).isEqualTo(USER_NEW);
+    }
+
+    @Test
+    void shouldFindUserById() {
+        var result = userRepositorySUT.findById(1L);
+
+        assertThat(result)
+                .isPresent()
+                .get().isEqualTo(USER_TEST1);
+    }
+
+    @Test
+    void shouldFindAllUsers() {
+        var result = userRepositorySUT.findAll();
+
+        assertThat(result)
+                .isNotEmpty()
+                .contains(USER_TEST1, USER_TEST2);
+    }
+
+    @Test
+    void shouldUpdateExistingUser() {
+        User userTest01Updated = new User(1L, "jdoe", "Jossua", "Doe");
+        userRepositorySUT.update(userTest01Updated);
+
+        var result = userRepositorySUT.findById(1L);
+
+        assertThat(result)
+                .isPresent()
+                .get().isEqualTo(userTest01Updated);
+    }
+
+    @Test
+    void shouldDeleteUserById() {
+        userRepositorySUT.deleteById(1L);
+
+        var result = userRepositorySUT.findById(1L);
+        assertThat(result).isNotPresent();
+    }
+
+}
---
+++ micronaut-postgresql-tile-example/src/test/resources/db/data/R__001_initial_data_test.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, username, name, surename)
+VALUES
+    (1, 'jdoe', 'John', 'Doe'),
+    (2, 'asmith', 'Addam', 'Smith');
+
