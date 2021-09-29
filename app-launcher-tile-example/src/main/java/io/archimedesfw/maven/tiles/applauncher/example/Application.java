99bf2540301d17e5f551eabf364b8f9548e1e2f9
cgmarcos <cgmarcos@autentia.com>
2021-09-29 13:19:26 +0200

    Initial App-Launcher Tile example

---
+++ app-launcher-tile-example/src/main/java/io/archimedesfw/maven/tiles/applauncher/example/Application.java
@@ -0,0 +1,10 @@
+package io.archimedesfw.maven.tiles.applauncher.example;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
---
+++ app-launcher-tile-example/src/main/java/io/archimedesfw/maven/tiles/applauncher/example/delivery/rest/UserController.java
@@ -0,0 +1,23 @@
+package io.archimedesfw.maven.tiles.applauncher.example.delivery.rest;
+
+import io.archimedesfw.maven.tiles.applauncher.example.delivery.rest.vo.UserResponse;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+import java.util.List;
+
+@Controller("/user")
+public class UserController {
+
+    private static final UserResponse USER_001 = new UserResponse(1L, "jdoe", "John", "Doe");
+    private static final UserResponse USER_002 = new UserResponse(2L, "ffernandez", "Federico", "Fernandez");
+
+    @Get
+    public HttpResponse<List<UserResponse>> findAllUsers(){
+        List<UserResponse> result = List.of(USER_001, USER_002);
+
+        return HttpResponse.ok(result);
+    }
+
+}
